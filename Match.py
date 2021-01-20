f72e2ffef6e5959db221c573d6ea68b650f46a0a
Stepowskieric <Stepowskieric@gmail.com>
2021-01-20 14:35:45 -0500

    added all backend python

---
+++ Match.py
@@ -0,0 +1,14 @@
+from User import User
+
+
+class Match:
+    def __init__(self,outcome,owMap,user):
+        self.owMap = map
+        self.outcome = outcome
+        self.user = User()
+
+
+    def createusers(self,):
+
+
+
---
+++ User.py
@@ -0,0 +1,10 @@
+
+
+class User:
+    def __init__(self,nickname,role,heroesplayed):
+        self.heroesPlayed = heroesplayed
+        self.nickname = nickname
+        self.role = role
+
+
+
